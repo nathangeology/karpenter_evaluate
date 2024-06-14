56491e27a26fcfa9a45735e4a6a5b0792e31f340
Nathaniel Jones <jonesflp@amazon.com>
2024-06-14 14:46:46 -0700

    Changed to look at all csvs in directory

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import sys
 from kapenter_evaluate import get_metrics
+import os
 
 
 if __name__ == "__main__":
@@ -7,5 +8,9 @@ if __name__ == "__main__":
         print("Usage: python script.py <filename>")
         sys.exit(1)
 
-    filename = sys.argv[1]
-    get_metrics(filename)
+    csv_dir = sys.argv[1]
+    files = os.listdir(csv_dir)
+    # Filter the list to include only CSV files
+    csv_files = [file for file in files if file.endswith('.csv')]
+    for csv_file in csv_files:
+        get_metrics(csv_file)
