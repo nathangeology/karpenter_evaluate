371660bd776add003f6d60cedfd7c2c999b67737
Nathaniel Jones <jonesflp@amazon.com>
2024-06-14 13:30:57 -0700

    Added main function to collect metrics based on an output csv file that has the timestamps

---
+++ main.py
@@ -0,0 +1,11 @@
+import sys
+from kapenter_evaluate import get_metrics
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: python script.py <filename>")
+        sys.exit(1)
+
+    filename = sys.argv[1]
+    get_metrics(filename)
