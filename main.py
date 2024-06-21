53f02e6821ee14c7606947352295b72f37a19a86
Nathaniel Jones <jonesflp@amazon.com>
2024-06-21 16:31:50 -0700

    Changed the input method for the kpi main.py function

--- main.py
+++ main.py
@@ -4,11 +4,9 @@ import os
 
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: python script.py <filename>")
-        sys.exit(1)
-
-    csv_dir = sys.argv[1]
+    csv_dir = os.environ['TEMP_DIR']
+    if csv_dir[-1] != '/':
+        csv_dir += '/'
     files = os.listdir(csv_dir)
     # Filter the list to include only CSV files
     csv_files = [file for file in files if file.endswith('.csv')]
